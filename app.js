9049910dc7ea41553f2e60a5f9849a2904bed3b6
Jia Rong Tsai <jiarongtsai19@gmail.com>
2021-12-22 14:20:18 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
+const Todo = require('./models/todo')
+
 mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true, 
     useUnifiedTopology: true})
 
@@ -30,10 +32,18 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
-
 app.get('/', (req, res)=>{
-    res.render('index')
+    res.render('intro')
+})
+
+app.get('/todo', (req, res)=>{
+    Todo.find() //取出資料
+    .lean() //Mongoose model to JS object
+    .then(
+        todos => res.render('index', {todos}) //傳資料給 index
+    ).catch(
+        e => console.error(e)
+    )
 })
 
 app.listen(3000, ()=>{
