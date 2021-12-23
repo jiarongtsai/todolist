d9cc798090e2aefd3d69a34d5b8e8d64b8de95d3
Jia Rong Tsai <jiarongtsai19@gmail.com>
2021-12-23 12:08:50 +0800

    feat: added edit page

--- app.js
+++ app.js
@@ -75,6 +75,30 @@ app.get('/todos/:id', (req, res)=>{
  
 })
 
+//edit todo
+app.get('/todos/:id/edit', (req, res)=>{
+
+    const id = req.params.id
+    return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+//update todo
+app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id)
+      .then(todo => {
+        todo.name = name
+        return todo.save()
+      })
+      .then(()=> res.redirect(`/todos/${id}`))
+      .catch(error => console.log(error))
+  })
+
+
 app.listen(3000, ()=>{
     console.log('App is running on http://localhost:3000!')
 })
